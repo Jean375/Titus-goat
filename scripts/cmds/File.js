0391d99c102ef375f1cb9ca6553fd7524604e3ba
Jean375 <shibaiotsutsuki14@gmail.com>
2024-09-14 10:07:16 +0100

    Create File.js

---
+++ scripts/cmds/File.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "file",
+    version: "1.0",
+    author: "OtinXShiva",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["100057265804084"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("dégage créature inférieur__ತ⁠_⁠ʖ⁠ತ seul 🔰_jean_noël_🔰 mon maître utilise cette commande...🉑✍️", event.threadID, event.messageID);
+    }
+    
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("𝖡𝖺𝗅𝖺𝗇𝖼𝖾 𝗅𝖾 𝗇𝗈𝗆 𝖽𝗎 𝖿𝗂𝖼𝗁𝗂𝖾𝗋.", event.threadID, event.messageID);
+    }
+
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
